321b386b48b57db6181176f9cec982f331e0c7f0
Aslan <berezg86@gmail.com>
2019-04-26 13:15:20 +0300

    Структура проекта, шрифты

---
+++ js/common.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+
+    // Mobile menu =============================================================
+    // $(function() {
+    //     function burger() {
+    //         $('.main-nav').toggleClass('nav-show');
+    //     };
+    //     $('.burger-menu').click(function() {
+    //         burger();
+    //     });
+    //     $('.close').click(function() {
+    //         $('.main-nav').removeClass('nav-show');
+    //     });
+    //     $(window).resize(function() {
+    //         var w = $(window).width();
+    //         if (w >= 768) {
+    //             $('.main-nav').removeAttr('style');
+    //         }
+    //     });
+    // });
+
+    // send message ============================================================
+    // $(".form-reserv").submit(function () {
+    //     var inpFirst = $(this).find('.inp_first');
+    //     var inpSecond = $(this).find('.inp_second');
+    //     var inpFirstInner = $(this).find('.inp_first input');
+    //     var inpSecondInner = $(this).find('.inp_second input');
+    //
+    //     var emptyFirst = false;
+    //     var emptySecond = false;
+    //
+    //     if (inpFirstInner.val() == "") {
+    //         emptyFirst = true;
+    //     }
+    //     if(inpSecondInner.val() == ""){
+    //         var emptySecond = true;
+    //     }
+    //     if (emptyFirst == true) {
+    //         inpFirst.addClass("error-input");
+    //         inpFirstInner.focus();
+    //     }
+    //     if (emptySecond == true) {
+    //         inpSecond.addClass("error-input");
+    //         inpSecondInner.focus();
+    //     }else{
+    //         var form_data = $(this).serialize();
+    //         $.ajax({
+    //             type: "POST",
+    //             url: "/sendmessage.php",
+    //             data: form_data,
+    //             success: function () {
+    //                 cleanTnanks(this);
+    //             }
+    //         });
+    //     }
+    //     return false;
+    // });
+    // function cleanTnanks(form) {
+    //     $('.inp_first').removeClass("error-input");
+    //     $('.inp_second').removeClass("error-input");
+    //
+    //     $(".inp_first input").val("");
+    //     $(".inp_second input").val("");
+    //
+    //     $('.reservation-modal').hide();
+    //     $('.modal-thanks').fadeIn();
+    // };
+    //
+    // // custom select ===========================================================
+    // $('select').selectric();
+    //
+    // // modal reservation =======================================================
+    // $('.modal-show').click(function() {
+    //     $('.overlay').fadeIn();
+    //     $('.reservation-modal').fadeIn();
+    //     $('.close-modal').click(function() {
+    //         $('.overlay').fadeOut();
+    //         $('.reservation-modal').fadeOut();
+    //         $('.modal-thanks').fadeOut();
+    //     });
+    //     $('.overlay').click(function() {
+    //         $('.overlay').fadeOut();
+    //         $('.reservation-modal').fadeOut();
+    //         $('.modal-thanks').fadeOut();
+    //     });
+    // });
+    //
+    // // slider ==================================================================
+    // var swiper = new Swiper('.swiper-container', {
+    //     slidesPerView: 'auto',
+    //     //   spaceBetween: 83,
+    //     loop: true,
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         clickable: true,
+    //     },
+    // });
+
+});
