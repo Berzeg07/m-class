da2d7099aeafdf676faa930097cbf9ad081741f0
Aslan <berezg86@gmail.com>
2019-05-12 11:14:10 +0300

    верстка блога

--- js/common.js
+++ js/common.js
@@ -1,5 +1,52 @@
 $(document).ready(function() {
 
+    (function() {
+        var a = document.querySelector('#airSticky'),
+            b = null,
+            P = 50; // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
+        window.addEventListener('scroll', Ascroll, false);
+        document.body.addEventListener('scroll', Ascroll, false);
+
+        function Ascroll() {
+            if (b == null) {
+                var Sa = getComputedStyle(a, ''),
+                    s = '';
+                for (var i = 0; i < Sa.length; i++) {
+                    if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+                        s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; '
+                    }
+                }
+                b = document.createElement('div');
+                b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+                a.insertBefore(b, a.firstChild);
+                var l = a.childNodes.length;
+                for (var i = 1; i < l; i++) {
+                    b.appendChild(a.childNodes[1]);
+                }
+                a.style.height = b.getBoundingClientRect().height + 'px';
+                a.style.padding = '0';
+                a.style.border = '0';
+            }
+            var Ra = a.getBoundingClientRect(),
+                R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.lettersBlock').getBoundingClientRect().top + 50); // селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
+            if ((Ra.top - P) <= 0) {
+                if ((Ra.top - P) <= R) {
+                    b.className = 'stop';
+                    b.style.top = -R + 'px';
+                } else {
+                    b.className = 'sticky';
+                    b.style.top = P + 'px';
+                }
+            } else {
+                b.className = '';
+                b.style.top = '';
+            }
+            window.addEventListener('resize', function() {
+                a.children[0].style.width = getComputedStyle(a, '').width
+            }, false);
+        }
+    })();
+
     $('.contTabs a').click(function(e) {
         e.preventDefault();
         $('.contTabs a').removeClass('active');
@@ -8,9 +55,9 @@ $(document).ready(function() {
         $('.contTabs-cont').not(tab).css({
             'display': 'none'
         });
-        $(tab).css('display','block');
+        $(tab).css('display', 'block');
     });
-    $('.contTabs a:first').click();
+    $('.contTabs a:last').click();
 
     $('.programsArticle-tabs a').click(function(e) {
         e.preventDefault();
@@ -20,7 +67,7 @@ $(document).ready(function() {
         $('.programsArticle-tabs_cont').not(tab).css({
             'display': 'none'
         });
-        $(tab).css('display','block');
+        $(tab).css('display', 'block');
     });
     $('.programsArticle-tabs a:first').click();
 
@@ -238,6 +285,8 @@ $(document).ready(function() {
     new WOW().init();
 
     $(".schedule-inp_phone").mask("+ 7 (999) 999 - 99 - 99?");
+    $(".recallForm-phone").mask("+ 7 (999) 999 - 99 - 99?");
+
 
     // Mobile menu =============================================================
     $(".burger").click(function() {
